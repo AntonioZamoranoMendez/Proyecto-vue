185e59c450bb2751c6054af8b25346ad1600b5dd
SrOnionDeluxe <a23311011@uthermosillo.edu.mx>
2025-01-28 09:55:48 -0600

    lista de tareas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <contador></contador>
+  <ListaDeTareas></ListaDeTareas>
 </template>
 
 <script setup>
-import contador from './components/contador.vue'
+import contador from './components/Contador.vue'
+import ListaDeTareas from './components/ListaDeTareas.vue'
 </script>
 
 <style scoped></style>
